3caa41c174bcb2b9045b7d6561495d7a58a0d9c1
ashik2765 <ashikurrahman2765@gmail.com>
2024-12-02 23:22:45 +0600

    fixed the login page error

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,38 +1,44 @@
-"use client"
-import Image from 'next/image'
-import { signIn } from 'next-auth/react'
+"use client";
+import Image from 'next/image';
+import { signIn } from 'next-auth/react';
 import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
 import SocialLogin from '@/components/shared/SocialLogin';
 
-
 export default function LoginPage() {
     const router = useRouter();
     const searchParam = useSearchParams();
-    const path = searchParam.get('redirect')
+    const path = searchParam.get('redirect');
+
     const loginHandler = async (e) => {
         e.preventDefault();
         const form = e.target;
         const email = form.email.value;
         const password = form.password.value;
+
         const resp = await signIn("credentials", {
             email,
             password,
             redirect: true,
-            callbackUrl: path ? path : '/'
-        })
+            callbackUrl: path ? path : '/',
+        });
+
         if (resp.status === 200) {
-            router.push('/')
+            router.push('/');
         }
+    };
 
-
-    }
     return (
         <div className="container mx-auto">
             <div className="hero bg-base-200 min-h-screen">
                 <div className="hero-content flex-col lg:flex-row gap-10">
                     <div className="text-center lg:text-left">
-                        <Image src="/assets/images/login/login.svg" width={460} height={502} alt='Login Image' />
+                        <Image
+                            src="/assets/images/login/login.svg"
+                            width={460}
+                            height={502}
+                            alt="Login Image"
+                        />
                     </div>
                     <div className="card bg-base-100 w-full max-w-lg shrink-0 shadow-2xl">
                         <form onSubmit={loginHandler} className="card-body">
@@ -41,30 +47,48 @@ export default function LoginPage() {
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input name="email" type="email" placeholder="email" className="input input-bordered" required />
+                                <input
+                                    name="email"
+                                    type="email"
+                                    placeholder="email"
+                                    className="input input-bordered"
+                                    required
+                                />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input name="password" type="password" placeholder="password" className="input input-bordered" required />
+                                <input
+                                    name="password"
+                                    type="password"
+                                    placeholder="password"
+                                    className="input input-bordered"
+                                    required
+                                />
                                 <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                    <a href="#" className="label-text-alt link link-hover">
+                                        Forgot password?
+                                    </a>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Login</button>
                             </div>
-
                         </form>
-                        <div className='text-center'>
+                        <div className="text-center">
                             <p>Or Sign In with</p>
-                            <SocialLogin></SocialLogin>
-                            <p className="mt-5">Don't have an account? <Link href="/signUp"><span className='text-primary'>Sign Up</span></Link></p>
+                            <SocialLogin />
+                            <p className="mt-5">
+                                Don&apos;t have an account?{' '}
+                                <Link href="/signUp">
+                                    <span className="text-primary">Sign Up</span>
+                                </Link>
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
